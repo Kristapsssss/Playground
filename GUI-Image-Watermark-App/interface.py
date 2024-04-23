d3267a20826ab834ed922f1d6ccc0772b86a6d16
Kristapsssss <165655894+Kristapsssss@users.noreply.github.com>
2024-04-23 17:45:05 +0300

    Add customtkinter widgets

--- GUI-Image-Watermark-App/interface.py
+++ GUI-Image-Watermark-App/interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import customtkinter
 
 from main2 import Watermarker
 app = Watermarker()
@@ -34,28 +35,28 @@ remote_frame.grid(row=1, column=1, padx=10, pady=10)
 
 # Create and locate widgets inside image_frame
 label_image = tk.Canvas(image_frame, width=1500, height=900, borderwidth=0, bg="darkseagreen", highlightthickness=0)
-button_open = tk.Button(image_frame, text='Open Image From File Directory', command=lambda: app.open_image(label_image), font=('arial', 13, 'bold'))
-button_save = tk.Button(image_frame, text='Save', command=lambda: app.save_image(canvas=label_image), font=('arial', 13))
+button_open = customtkinter.CTkButton(image_frame, width=50, height=50, text='Open Image From File Directory', command=lambda: app.open_image(label_image), font=('arial', 20, 'bold'))
+button_save = customtkinter.CTkButton(image_frame, width=50, height=50, text='Save', command=lambda: app.save_image(canvas=label_image), font=('arial', 20), fg_color=('darkgreen', 'darkgreen'))
 
 label_image.grid(row=0, column=0, columnspan=2)
 button_open.grid(row=1, column=0, pady=75)
 button_save.grid(row=1, column=1)
 
 # Create and locate widgets inside control_frame
-button_watermark_open = tk.Button(control_frame, text='Select Watermark Image', command=app.draw_watermark, font=('arial', 11, 'bold'))
-button_delete_watermark = tk.Button(control_frame, text='Remove Watermark', command=app.delete_watermark, font=('arial', 11))
+button_watermark_open = customtkinter.CTkButton(control_frame, width=50, height=50, text='Select Watermark Image', command=app.draw_watermark, font=('arial', 20, 'bold'))
+button_delete_watermark = customtkinter.CTkButton(control_frame, width=50, height=50, text='Remove Watermark', command=app.delete_watermark, font=('arial', 15), fg_color=('darkred', 'darkred'))
 
 label_watermark_text = tk.Label(control_frame, text='Watermark Text:', bg="powderblue", font=('arial', 13))
-watermark_text = tk.Entry(control_frame)
+watermark_text = customtkinter.CTkEntry(control_frame)
 
-button_add_text = tk.Button(control_frame, text='Add', command=lambda: [app.draw_text(watermark_text), app.get_font(combobox_font), app.get_color(combobox_color)], font=('arial', 13))
-button_remove_text = tk.Button(control_frame, text='Remove', command=app.remove_text, font=('arial', 13))
+button_add_text = customtkinter.CTkButton(control_frame, width=50, height=50, text='Add', command=lambda: [app.draw_text(watermark_text), app.get_font(combobox_font), app.get_color(combobox_color)], font=('arial', 15))
+button_remove_text = customtkinter.CTkButton(control_frame, width=50, height=50, text='Remove', command=app.remove_text, font=('arial', 15), fg_color=('darkred', 'darkred'))
 
 label_font = tk.Label(control_frame, text="Select Text Font:", bg="powderblue", font=('arial', 13))
-combobox_font = tk.ttk.Combobox(control_frame, values=["arial", "arialbd", "BASKVILL", "BRADHITC", "cambriab", "FREESCPT", "ITCBLKAD", "unispace bd", "verdana", "TEMPSITC"])
+combobox_font = customtkinter.CTkComboBox(control_frame, values=["arial", "arialbd", "BASKVILL", "BRADHITC", "cambriab", "FREESCPT", "ITCBLKAD", "unispace bd", "verdana", "TEMPSITC"])
 
 label_font_color = tk.Label(control_frame, text="Select Text Color:", bg="powderblue", font=('arial', 13))
-combobox_color = tk.ttk.Combobox(control_frame, values=["antiquewhite", 'black', 'tomato', 'pink', 'purple', 'wheat', 'springgreen', 'powderblue', 'blanchedalmond', 'hotpink', 'magenta'])
+combobox_color = customtkinter.CTkComboBox(control_frame, values=["antiquewhite", 'black', 'tomato', 'pink', 'purple', 'wheat', 'springgreen', 'powderblue', 'blanchedalmond', 'hotpink', 'magenta'])
 
 button_watermark_open.grid(row=0, column=0, pady=100)
 button_delete_watermark.grid(row=0, column=1)
@@ -67,10 +68,10 @@ button_add_text.grid(row=4, column=0, pady=10)
 button_remove_text.grid(row=4, column=1, pady=10)
 
 label_font.grid(row=2, column=0, pady=5)
-combobox_font.grid(row=2, column=1, padx=10)
+combobox_font.grid(row=2, column=1, padx=10, pady=10)
 
 label_font_color.grid(row=3, column=0, pady=5)
-combobox_color.grid(row=3, column=1)
+combobox_color.grid(row=3, column=1, pady=10)
 
 # Remote Frame Widgets
 label_watermark_size = tk.Label(remote_frame, text='Size', bg="powderblue", font=('arial', 13, 'bold'))
@@ -83,7 +84,7 @@ button_down = tk.Button(remote_frame, image=tk_down_arrow, relief='flat', bd=0, 
 button_left = tk.Button(remote_frame, image=tk_left_arrow, relief='flat', bd=0, command=app.watermark_left, bg="powderblue", activebackground="powderblue")
 button_right = tk.Button(remote_frame, image=tk_right_arrow, relief='flat', bd=0, command=app.watermark_right, bg="powderblue", activebackground="powderblue")
 
-button_exit = tk.Button(remote_frame, text='Exit', command=root.destroy, font=('arial', 13))
+button_exit = customtkinter.CTkButton(remote_frame, width=50, height=50, text='Exit', command=root.destroy, font=('arial', 20), fg_color=('darkred', 'darkred'))
 
 label_watermark_size.grid(row=3, column=1, pady=100)
 button_bigger.grid(row=3, column=0, sticky='e')
